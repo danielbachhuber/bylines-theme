8ea4f698d8d565417eda887cbda63d461e5ce059
Daniel Bachhuber <daniel@handbuilt.co>
2017-05-02 07:15:03 -0700

    Add Drip tag

--- header.php
+++ header.php
@@ -7,6 +7,24 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
+
+<?php if ( 'bylines.io' === parse_url( home_url(), PHP_URL_HOST ) ) : ?>
+<!-- Drip -->
+<script type="text/javascript">
+  var _dcq = _dcq || [];
+  var _dcs = _dcs || {}; 
+  _dcs.account = '3444075';
+
+  (function() {
+    var dc = document.createElement('script');
+    dc.type = 'text/javascript'; dc.async = true; 
+    dc.src = '//tag.getdrip.com/3444075.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(dc, s);
+  })();
+</script>
+<?php endif; ?>
+
 </head>
 
 <body <?php body_class(); ?>>
